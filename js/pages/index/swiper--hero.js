eb79c50bd88e0cdde5ccdf9f3341f262f5c73b27
1103syike <1103syike@gmail.com>
2026-01-16 01:10:48 +0800

    fix(index,product): 修復部分圖片無法正確顯示的問題。

--- js/pages/index/swiper--hero.js
+++ js/pages/index/swiper--hero.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const slides = [
-        { id: 1, name: '輪播圖-1', srcLeft: '/image/page/index/swiper--hero__img-1--left.jpg', srcRight: '/image/page/index/swiper--hero__img-1--right.jpg' },
-        { id: 2, name: '輪播圖-2', srcLeft: '/image/page/index/swiper--hero__img-2--left.jpg', srcRight: '/image/page/index/swiper--hero__img-2--right.jpg' },
-        { id: 3, name: '輪播圖-3', srcLeft: '/image/page/index/swiper--hero__img-3--left.jpg', srcRight: '/image/page/index/swiper--hero__img-3--right.jpg' },
-        { id: 4, name: '輪播圖-4', srcLeft: '/image/page/index/swiper--hero__img-4--left.jpg', srcRight: '/image/page/index/swiper--hero__img-4--right.jpg' },
-        { id: 5, name: '輪播圖-5', srcLeft: '/image/page/index/swiper--hero__img-5--left.jpg', srcRight: '/image/page/index/swiper--hero__img-5--right.jpg' }
+        { id: 1, name: '輪播圖-1', srcLeft: './image/page/index/swiper--hero__img-1--left.jpg', srcRight: './image/page/index/swiper--hero__img-1--right.jpg' },
+        { id: 2, name: '輪播圖-2', srcLeft: './image/page/index/swiper--hero__img-2--left.jpg', srcRight: './image/page/index/swiper--hero__img-2--right.jpg' },
+        { id: 3, name: '輪播圖-3', srcLeft: './image/page/index/swiper--hero__img-3--left.jpg', srcRight: './image/page/index/swiper--hero__img-3--right.jpg' },
+        { id: 4, name: '輪播圖-4', srcLeft: './image/page/index/swiper--hero__img-4--left.jpg', srcRight: './image/page/index/swiper--hero__img-4--right.jpg' },
+        { id: 5, name: '輪播圖-5', srcLeft: './image/page/index/swiper--hero__img-5--left.jpg', srcRight: './image/page/index/swiper--hero__img-5--right.jpg' }
     ]
     const $ = (s) => document.querySelector(s)
 
